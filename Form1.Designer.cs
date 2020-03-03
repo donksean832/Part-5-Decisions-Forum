18a10e7e8c545651262c73e015ea2c709c1ef654
Sean Donker <DonkSean832@GoTVDSB.ca>
2020-03-03 13:50:32 -0500

    ahh

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,15 @@
             this.LblAge = new System.Windows.Forms.Label();
             this.LblAnswer = new System.Windows.Forms.Label();
             this.BtnAnswer = new System.Windows.Forms.Button();
+            this.BtnHurricane = new System.Windows.Forms.Button();
+            this.LblHurricane = new System.Windows.Forms.Label();
+            this.TxtHurricane = new System.Windows.Forms.TextBox();
+            this.LblHurricaneAnswer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TxtAge
             // 
-            this.TxtAge.Location = new System.Drawing.Point(187, 71);
+            this.TxtAge.Location = new System.Drawing.Point(179, 21);
             this.TxtAge.Name = "TxtAge";
             this.TxtAge.Size = new System.Drawing.Size(199, 22);
             this.TxtAge.TabIndex = 0;
@@ -44,7 +48,7 @@
             // LblAge
             // 
             this.LblAge.AutoSize = true;
-            this.LblAge.Location = new System.Drawing.Point(63, 67);
+            this.LblAge.Location = new System.Drawing.Point(23, 21);
             this.LblAge.Name = "LblAge";
             this.LblAge.Size = new System.Drawing.Size(105, 17);
             this.LblAge.TabIndex = 1;
@@ -53,27 +57,66 @@
             // LblAnswer
             // 
             this.LblAnswer.AutoSize = true;
-            this.LblAnswer.Location = new System.Drawing.Point(74, 173);
+            this.LblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LblAnswer.Location = new System.Drawing.Point(192, 71);
             this.LblAnswer.Name = "LblAnswer";
-            this.LblAnswer.Size = new System.Drawing.Size(11, 17);
+            this.LblAnswer.Size = new System.Drawing.Size(0, 25);
             this.LblAnswer.TabIndex = 2;
-            this.LblAnswer.Text = "!";
             // 
             // BtnAnswer
             // 
-            this.BtnAnswer.Location = new System.Drawing.Point(255, 167);
+            this.BtnAnswer.Location = new System.Drawing.Point(26, 66);
             this.BtnAnswer.Name = "BtnAnswer";
-            this.BtnAnswer.Size = new System.Drawing.Size(183, 58);
+            this.BtnAnswer.Size = new System.Drawing.Size(90, 40);
             this.BtnAnswer.TabIndex = 3;
             this.BtnAnswer.Text = "Answer";
             this.BtnAnswer.UseVisualStyleBackColor = true;
             this.BtnAnswer.Click += new System.EventHandler(this.BtnAnswer_Click);
+            // 
+            // BtnHurricane
+            // 
+            this.BtnHurricane.Location = new System.Drawing.Point(26, 243);
+            this.BtnHurricane.Name = "BtnHurricane";
+            this.BtnHurricane.Size = new System.Drawing.Size(85, 45);
+            this.BtnHurricane.TabIndex = 4;
+            this.BtnHurricane.Text = "Hurricane Catagory";
+            this.BtnHurricane.UseVisualStyleBackColor = true;
+            this.BtnHurricane.Click += new System.EventHandler(this.BtnHurricane_Click);
+            // 
+            // LblHurricane
+            // 
+            this.LblHurricane.AutoSize = true;
+            this.LblHurricane.Location = new System.Drawing.Point(23, 185);
+            this.LblHurricane.Name = "LblHurricane";
+            this.LblHurricane.Size = new System.Drawing.Size(128, 17);
+            this.LblHurricane.TabIndex = 5;
+            this.LblHurricane.Text = "Enter windspeed in";
+            // 
+            // TxtHurricane
+            // 
+            this.TxtHurricane.Location = new System.Drawing.Point(179, 182);
+            this.TxtHurricane.Name = "TxtHurricane";
+            this.TxtHurricane.Size = new System.Drawing.Size(199, 22);
+            this.TxtHurricane.TabIndex = 6;
+            // 
+            // LblHurricaneAnswer
+            // 
+            this.LblHurricaneAnswer.AutoSize = true;
+            this.LblHurricaneAnswer.Location = new System.Drawing.Point(194, 257);
+            this.LblHurricaneAnswer.Name = "LblHurricaneAnswer";
+            this.LblHurricaneAnswer.Size = new System.Drawing.Size(46, 17);
+            this.LblHurricaneAnswer.TabIndex = 7;
+            this.LblHurricaneAnswer.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblHurricaneAnswer);
+            this.Controls.Add(this.TxtHurricane);
+            this.Controls.Add(this.LblHurricane);
+            this.Controls.Add(this.BtnHurricane);
             this.Controls.Add(this.BtnAnswer);
             this.Controls.Add(this.LblAnswer);
             this.Controls.Add(this.LblAge);
@@ -91,6 +134,10 @@
         private System.Windows.Forms.Label LblAge;
         private System.Windows.Forms.Label LblAnswer;
         private System.Windows.Forms.Button BtnAnswer;
+        private System.Windows.Forms.Button BtnHurricane;
+        private System.Windows.Forms.Label LblHurricane;
+        private System.Windows.Forms.TextBox TxtHurricane;
+        private System.Windows.Forms.Label LblHurricaneAnswer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,38 +20,76 @@ namespace Part_5_Decisions_Forum
         private void BtnAnswer_Click(object sender, EventArgs e)
         {
             int age;
-            //age = Convert.ToInt32(TxtAge.Text);
-            if (Int32.TryParse(TxtAge.Text, out age) && age > 18)
-                LblAnswer.Text = "Adult";
-            else if (age <= 18)
-            {
-                if (age <= 12)
-                {
-                    if (age <= 10)
-                    {
-                        if (age <= 5)
-                        {
-                            if (age <= 0)
-                            {
-                                LblAnswer.Text = "Error: You arent born yet";
-                            }
 
+            if (Int32.TryParse(TxtAge.Text, out age))
+            {
+                if (age > 18)
+                    LblAnswer.Text = "Adult";
+                else if (age <= 18)
+                {
+                    if (age <= 12)
+                    {
+                        if (age <= 10)
+                        {
+                            if (age <= 5)
+                            {
+                                if (age <= 0)
+                                {
+                                    LblAnswer.Text = "Error: You arent born yet";
+                                }
+
+                                else
+                                    LblAnswer.Text = "Toddler";
+                            }
                             else
-                                LblAnswer.Text = "Toddler";
+                                LblAnswer.Text = "Child";
                         }
                         else
-                            LblAnswer.Text = "Child";
+                            LblAnswer.Text = "Preteen";
                     }
                     else
-                        LblAnswer.Text = "Preteen";
+                        LblAnswer.Text = "Teen";
                 }
-                else
-                    LblAnswer.Text = "Teen";
             }
-          
-                              
-            
+            else
+                LblAnswer.Text = "Error: Enter an integer";
 
+
+
+            //Color c = (Color)(new Random()).Next(0, 3);
+            //switch (c)
+            //{
+            //    case Color.Red:
+            //        Console.WriteLine("The color is red");
+            //        break;
+            //    case Color.Green:
+            //        Console.WriteLine("The color is green");
+            //        break;
+            //    case Color.Blue:
+            //        Console.WriteLine("The color is blue");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The color is unknown.");
+            //        break;
+            }
+
+        private void BtnHurricane_Click(object sender, EventArgs e)
+        {
+            int windspeed;
+            if (Int32.TryParse(TxtHurricane.Text, out windspeed))
+            {
+                switch (windspeed)
+                {
+                    case 1:
+                        LblHurricaneAnswer.Text = ($"Hello");
+                        break;
+                    case 
+                }
+
+            }
+            else
+                LblHurricaneAnswer.Text = "Error: Enter Integer";
+                
         }
     }
 }
